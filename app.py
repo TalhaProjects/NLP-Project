b782ba9f22e3c81cb6e5197ee2fd70162f6c3651
TalhaProjects <muhammadtalhaa290@gmail.com>
2025-12-06 18:44:22 +0500

    updated main file

--- app.py
+++ app.py
@@ -353,4 +353,4 @@ def api_info():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port, use_reloader=False)
